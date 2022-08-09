020cc4e95d8fc78e198f35c258211cebc369e341
Matt Pahuta <mhpahuta@gmail.com>
2022-08-09 15:55:10 -0700

    Add challenge for 8-9-2022

--- js-fundamentals.js
+++ js-fundamentals.js
@@ -26,3 +26,38 @@ function removeChar(str) {
   // return str.substring(1, str.length -1);
 
 };
+
+// console.log(removeChar("HHello Worldd"))
+// console.log(removeChar("Mass Effectt"))
+// console.log(removeChar("John"))
+// console.log(removeChar("Dog"))
+
+// const myStr = 'HHello Worldd';
+// const myStrArr = [];
+// for (let i = 0; i < myStr.length; i += 1) {
+//   myStrArr.push(myStr[i]);
+// }
+
+// console.log(myStrArr)
+// myStrArr.pop();
+// myStrArr.shift();
+// console.log(myStrArr);
+// const newStr = myStrArr.join('');
+// console.log(newStr);
+
+// *** Return Negative 
+
+function makeNegative(num) {
+  // if (num <= 0) {
+  //   return num;
+  // } else {
+  // return Math.abs(num) * -1;
+  // }
+  return num < 0 ? -num : num;
+
+}
+
+console.log(makeNegative(-5)); // return -5
+console.log(makeNegative(1)); // return -1
+console.log(makeNegative(0)); // return 0
+console.log(makeNegative(0.12)); // return -0.12

a328eee42ba23abd780005fe980fffe09d2be8d3
Matt Pahuta <mhpahuta@gmail.com>
2022-08-21 11:21:45 -0700

    Add challenge for 8-21-2022

--- addBinary.js
+++ addBinary.js
@@ -130,3 +130,17 @@ function countBy(x, n) {
   }
   return z;
 }
+
+// *** Sentence Smash - 8 kyu
+/*
+  Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
+  You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. 
+  Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+  Example:
+  ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+*/
+
+function smash(words) {
+  return words.join(' ').trim(); // use join to concatenate the words from the array, use trim to ensure no extra space, beginning or end
+}

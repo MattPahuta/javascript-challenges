a86ffed965b8883d19004c9bc28c3490a2750d7a
Matt Pahuta <mhpahuta@gmail.com>
2022-08-26 11:20:40 -0700

    Add challenge for 8-26-2022

--- addBinary.js
+++ addBinary.js
@@ -112,3 +112,21 @@ function getGrade(s1, s2, s3) {
 console.log(getGrade(95,90,93)) // => 'A'
 console.log(getGrade(84,79,85)) // => 'B'
 console.log(getGrade(65,70,59)) // => 'D'
+
+// *** Count by X - 8 kyu
+/*
+  Create a function with two arguments that will return an array of the first n multiples of x.
+  Assume both the given number and the number of times to count will be positive numbers greater than 0.
+  Return the results as an array or list ( depending on language ).
+
+  Examples
+  countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+  countBy(2,5) === [2,4,6,8,10]
+*/
+function countBy(x, n) {
+  let z = [];
+  for (let i = 1; i <= n; i++) { // i is your counter, counting up to and including 'n'
+    z.push(x*i) // for each iteration, push the value of x * 1
+  }
+  return z;
+}
